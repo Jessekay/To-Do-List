f9240697d5aa86ff6ec0d62a8b64e8693cc4afef
Jesse <kayjes00@gmail.com>
2023-04-07 23:51:34 +0200

    added styles for the to do list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import Style from './index.css';
+
+
+// Get DOM
+const listElement = document.createElement('div');
+listElement.classList = 'dynamic-container';
+const listContainer = document.querySelector('.list');
+
+const task = [
+  {
+    description: 'task1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'task4',
+    completed: false,
+    index: 4,
+  },
+];
