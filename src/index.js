5cd40df44ce889d9ee4c9aa75b816d801e4ba125
Jesse <kayjes00@gmail.com>
2023-04-08 12:05:07 +0200

    applied both add and remove items

--- src/index.js
+++ src/index.js
@@ -1,34 +1,66 @@
 import './index.css';
 
-// Get DOM
-let todoList = window.localStorage.getItem('todoList') ? JSON.parse(window.localStorage.getItem('todoList')) : []
-const inputItem = document.querySelector('.inputs-field')
-const listContainer = document.querySelector('.list')
-const listElement = document.createElement('div');
+let todoList = window.localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : [];
+const inputItem = document.querySelector('.inputs-field');
+const listContainer = document.querySelector('.list');
+const addButton = document.querySelector('.add-btn');
+const clearAll = document.querySelector('#clear-btn');
 
-const addButton = document.querySelector('.add-btn')
-const clearAll = document.querySelector('#clear-btn')
+const displayToDoList = () => {
+  listContainer.innerHTML = '';
+  todoList.forEach((item, index) => {
+    const listItem = document.createElement('div');
+    listItem.className = 'list-properties';
+    listItem.innerHTML = `
+    <div class="both-sides">
+      <span class="left-items">
+        <input type="checkbox" ${item.completed ? 'checked' : ''}>
+        <p>${item.description}</p>
+      </span>
+      <span class="right-items">
+        <div id="remove-single"><i class="fa-solid fa-trash"></i></div>
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+      </span>
+      </div>
+    `;
+    listItem.querySelector('#remove-single').addEventListener('click', (event) => {
+      event.preventDefault();
+      // eslint-disable-next-line no-use-before-define
+      removeItem(index);
+    });
+    listItem.querySelector('input[type="checkbox"]').addEventListener('change', (event) => {
+      event.preventDefault();
+      // eslint-disable-next-line no-use-before-define
+      markCompleted(index);
+    });
+    listContainer.appendChild(listItem);
+  });
+};
+
+const saveTodoList = () => {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+};
 const addItem = () => {
-    let task = inputItem.value;
-    if(task) {
-        let taskItem = {
-            description: task,
-            completed: false,
-        }
-        todoList.push(taskItem)
-        inputItem.value = '';
-        displayToDoList()
-        saveTodoList()
-    } 
-}
+  const task = inputItem.value;
+  if (task) {
+    const taskItem = {
+      description: task,
+      completed: false,
+    };
+    todoList.push(taskItem);
+    inputItem.value = '';
+    displayToDoList();
+    saveTodoList();
+  }
+};
 
 const markCompleted = (itemIndex) => {
-    if (itemIndex >= 0 && itemIndex < todoList.length) {
-        todoList[itemIndex].completed = true;
-        displayToDoList();
-        saveTodoList()
-    }
-}
+  if (itemIndex >= 0 && itemIndex < todoList.length) {
+    todoList[itemIndex].completed = true;
+    displayToDoList();
+    saveTodoList();
+  }
+};
 
 // Remove items
 
@@ -36,42 +68,7 @@ const removeItem = (itemIndex) => {
   if (itemIndex >= 0 && itemIndex < todoList.length) {
     todoList.splice(itemIndex, 1);
     displayToDoList();
-    saveTodoList
+    saveTodoList();
   }
-}
+};
 
-// Remove all completed tasks
-const removeCompletedItems = () => {
-  todoList = todoList.filter(item => !item.completed);
-  displayToDoList();
-  saveTodoList()
-} 
-const displayToDoList = () => {
-  listContainer.innerHTML = '';
-  todoList.forEach((item, index) => {
-    const listItem = document.createElement('div');
-    listItem.className = 'list-properties';
-    listItem.innerHTML = `
-      <span class="left-items">
-        <input type="checkbox" ${item.completed ? 'checked' : ''}>
-        <p>${item.description}</p>
-      </span>
-      <i class="fa-solid fa-ellipsis-vertical"></i>
-      <button id="remove-single">-</button>
-      <hr>
-    `;
-    listItem.querySelector('#remove-single').addEventListener('click', (event) => {
-      removeItem(index)
-    })
-    listItem.querySelector('input[type="checkbox"]').addEventListener('change', (event) => {
-      markCompleted(index);
-    });
-    listContainer.appendChild(listItem);
-  });
-  addButton.addEventListener('click', addItem)
-clearAll.addEventListener('click', removeCompletedItems)
-
-const saveTodoList = () => {
-  window.localStorage.setItem('todoList', JSON.stringify(todoList));
-}
-}
