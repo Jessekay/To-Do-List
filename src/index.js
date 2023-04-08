772924fdc0bfaf4601bd4271f20668ec6dc56a7f
Jesse <kayjes00@gmail.com>
2023-04-08 11:53:16 +0200

    updated index.js and index.html file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,77 @@
 import './index.css';
 
 // Get DOM
+let todoList = window.localStorage.getItem('todoList') ? JSON.parse(window.localStorage.getItem('todoList')) : []
+const inputItem = document.querySelector('.inputs-field')
+const listContainer = document.querySelector('.list')
 const listElement = document.createElement('div');
-listElement.classList = 'dynamic-container';
-const listContainer = document.querySelector('.list');
 
-const task = [
-  {
-    description: 'task1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'task2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'task3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'task4',
-    completed: false,
-    index: 4,
-  },
-];
-function iterateTasks() {
-  task.forEach((item) => {
-    listElement.innerHTML
-        += `
-        <div class="list-properties">
-            <span class="left-items">
-                <input type="checkbox">
-                <p>${item.description}</p>
-            </span>
-            <i class="fa-solid fa-ellipsis-vertical"></i>
-        </div>
-        <hr>
-        `;
-  });
-
-  listContainer.appendChild(listElement);
+const addButton = document.querySelector('.add-btn')
+const clearAll = document.querySelector('#clear-btn')
+const addItem = () => {
+    let task = inputItem.value;
+    if(task) {
+        let taskItem = {
+            description: task,
+            completed: false,
+        }
+        todoList.push(taskItem)
+        inputItem.value = '';
+        displayToDoList()
+        saveTodoList()
+    } 
 }
-iterateTasks();
+
+const markCompleted = (itemIndex) => {
+    if (itemIndex >= 0 && itemIndex < todoList.length) {
+        todoList[itemIndex].completed = true;
+        displayToDoList();
+        saveTodoList()
+    }
+}
+
+// Remove items
+
+const removeItem = (itemIndex) => {
+  if (itemIndex >= 0 && itemIndex < todoList.length) {
+    todoList.splice(itemIndex, 1);
+    displayToDoList();
+    saveTodoList
+  }
+}
+
+// Remove all completed tasks
+const removeCompletedItems = () => {
+  todoList = todoList.filter(item => !item.completed);
+  displayToDoList();
+  saveTodoList()
+} 
+const displayToDoList = () => {
+  listContainer.innerHTML = '';
+  todoList.forEach((item, index) => {
+    const listItem = document.createElement('div');
+    listItem.className = 'list-properties';
+    listItem.innerHTML = `
+      <span class="left-items">
+        <input type="checkbox" ${item.completed ? 'checked' : ''}>
+        <p>${item.description}</p>
+      </span>
+      <i class="fa-solid fa-ellipsis-vertical"></i>
+      <button id="remove-single">-</button>
+      <hr>
+    `;
+    listItem.querySelector('#remove-single').addEventListener('click', (event) => {
+      removeItem(index)
+    })
+    listItem.querySelector('input[type="checkbox"]').addEventListener('change', (event) => {
+      markCompleted(index);
+    });
+    listContainer.appendChild(listItem);
+  });
+  addButton.addEventListener('click', addItem)
+clearAll.addEventListener('click', removeCompletedItems)
+
+const saveTodoList = () => {
+  window.localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+}
